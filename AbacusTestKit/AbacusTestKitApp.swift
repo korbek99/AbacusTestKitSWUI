80f0883fe29bcd702dd81446d21e0c9f8b0eee3b
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2025-07-22 12:47:37 -0400

    Initial Commit

---
+++ AbacusTestKit/AbacusTestKitApp.swift
@@ -0,0 +1,20 @@
+//
+//  AbacusTestKitApp.swift
+//  AbacusTestKit
+//
+//  Created by Jose Preatorian on 22-07-25.
+//
+
+import SwiftUI
+
+@main
+struct AbacusTestKitApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
